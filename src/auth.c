8652d86ff173951363eeb6f9b39d86fc2030c651
Aron Bergman <bathingrad@gmail.com>
2018-05-17 23:17:04 +0200

    Create functions to handle user

---
+++ src/auth.c
@@ -0,0 +1,85 @@
+#include <configr.h>
+
+#include "prompt.h"
+
+int has_token()
+{
+	int has_token = 1;
+	FILE *config;
+	config = fopen(strcat(getenv("HOME"), "/.katconfig"), "r");
+	if (config)
+	{
+		configr *handle = configr_init();
+		configr_parse_stream(handle, config);
+
+		configr_key *key = configr_get_key(handle, "user", "token");
+		if (key != NULL)
+		{
+			has_token = 0;
+		}
+
+		configr_free(handle);
+
+		fclose(config);
+	}
+
+	return has_token;
+}
+
+char *get_user()
+{
+	char *user = NULL;
+
+	if (has_token() == 0)
+	{
+		FILE *config;
+		config = fopen(strcat(getenv("HOME"), "/.katconfig"), "r");
+
+		configr *handle = configr_init();
+		configr_parse_stream(handle, config);
+
+		configr_key *key = configr_get_key(handle, "user", "user");
+		if (key != NULL)
+		{
+			user = key->value;
+		}
+
+		configr_free(handle);
+
+		fclose(config);
+	}
+	else
+	{
+		/* The file doesn't exist */
+		user = prompt_text("Email: ");
+	}
+
+	return user;
+}
+
+char *get_passwd()
+{
+	char *passwd = NULL;
+
+	if (has_token() == 0)
+	{
+		FILE *config;
+		config = fopen(strcat(getenv("HOME"), "/.katconfig"), "r");
+
+		configr *handle = configr_init();
+		configr_parse_stream(handle, config);
+
+		configr_key *key = configr_get_key(handle, "user", "token");
+		passwd = key->value;
+
+		configr_free(handle);
+
+		fclose(config);
+	}
+	else
+	{
+		passwd = prompt_passwd("Password: ");
+	}
+
+	return passwd;
+}
---
+++ src/auth.h
@@ -0,0 +1,8 @@
+#ifndef __KAT_AUTH_H
+#define __KAT_AUTH_H
+
+extern int has_token();
+extern char *get_user();
+extern char *get_passwd();
+
+#endif /* __KAT_AUTH_H */
